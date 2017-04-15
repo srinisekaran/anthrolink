f3accdec3381897a1685e5f7d71e4dc36e5d78e1
ssrinivaas <sekaran.srinivaas@gmail.com>
2017-04-15 14:11:01 -0700

    Added Voltage Check Function

--- RF Detector/BasicReadVoltage/BasicReadVoltage.ino
+++ RF Detector/BasicReadVoltage/BasicReadVoltage.ino
@@ -1,17 +1,24 @@
 
-/*  Reading voltages and Responding
+/*  Reading voltages (*** MSP 430G2 ***)
  *  Anthrolink
+ *  Srinivaas Sekaran
+ *  
+ *  Modified routine to accomodate transition from Teensy LC to
+ *  TI Launchpad MSP 430G2
  */
 
-#define THRESH 1.2
-#define ANPIN A0 // Analog read pin
+#define VOLTAGE_MAX 1.5
+#define VOLTAGE_MIN 1.3
+#define ANPIN A7 // Analog read pin
 
-int ledPin = 13;
+// GREEN_LED or RED_LED
+const int ledPin = GREEN_LED; 
+void voltage_check(float);
 
 void setup() {
+  
   // Initialize serial communication at 9600 bps
-  Serial.begin(9600);   
-  analogReadAveraging(16);
+  Serial.begin(9600);  
   pinMode(ledPin, OUTPUT);
 
 }
@@ -20,18 +27,25 @@ void loop() {
   
   int sensorData = analogRead(ANPIN);
   delay(3);
-  float voltage = sensorData * (5.0 / 1024.0);
-  float offsetvoltage = voltage - 1.0;
-  Serial.println(offsetvoltage); 
-  
-  if (offsetvoltage > THRESH) {
-    digitalWrite(ledPin, HIGH);
+  float voltage = sensorData * (3.0 / 1023.0);  
+  Serial.println(voltage); 
+
+  voltage_check(voltage);
+
+}
+
+void voltage_check(float voltage_in) {
+
+  // Turn on LED for 50 ms if voltage is within a defined range
+  if ((voltage_in > VOLTAGE_MIN) && (voltage_in < VOLTAGE_MAX )) {
+      digitalWrite(ledPin, HIGH);
   }
 
-  // Turn off LED if voltage is below threshold
   delay(50);
+  
+  // Turn off LED if voltage is below threshold
   digitalWrite(ledPin, LOW);
- 
+  
 }
 
 
